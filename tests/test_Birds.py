aa179071a41252e5f8efac020a2b6af12e604a2f
Vozhdai Oleksandr <oleksander.vozhdai@gmail.com>
2025-04-06 16:46:55 +0300

    test hotfix

--- tests/test_Birds.py
+++ tests/test_Birds.py
@@ -5,12 +5,11 @@ here I create a dummy in each function to make sure there will be no problems,
 I understand that this leads to a dry violation,
 but I am so sure that nothing will break for sure
 """
-
-from Birds import Bird
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(
     os.path.dirname(__file__), '..')))
+from Birds import Bird
 
 
 def test_bird_initialization(dummy_sprite_sheet):
