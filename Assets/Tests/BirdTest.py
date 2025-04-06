fe5aa84cc05bdfbefe111e4d96a586ea5790dca4
Vozhdai Oleksandr <oleksander.vozhdai@gmail.com>
2025-04-06 15:27:19 +0300

    Test Update
    added files for testing, created a minimal architecture for tests. Added requirements and .gitignore. Created a file for fixtures, added a test for birds that checks their basic functions.

---
+++ Assets/Tests/BirdTest.py
@@ -0,0 +1,59 @@
+# test_bird.py
+
+#here I create a dummy in each function to make sure there will be no problems,
+#I understand that this leads to a dry violation, but I am so sure that nothing will break for sure
+
+
+from Birds import Bird
+from confest import dummy_sprite_sheet
+
+
+def test_bird_initialization(dummy_sprite_sheet):
+    bird = Bird(
+        value=10, x=100, y=200, speed=2, hp=3,
+        DirectionTimeChange=1.0,
+        right=True, left=False, up=True, down=False,
+        sprite_sheet=dummy_sprite_sheet,
+        SpritePerRow=4, SpriteWidth=32, SpriteHeight=32
+    )
+
+    assert bird.x == 100
+    assert bird.y == 200
+    assert bird.hp == 3
+    assert bird.alive is True
+    assert bird.sprite_sheet == dummy_sprite_sheet
+
+def test_bird_kill(dummy_sprite_sheet):
+    bird = Bird(
+        value=10, x=100, y=200, speed=2, hp=3,
+        DirectionTimeChange=1.0,
+        right=False, left=False, up=False, down=False,
+        sprite_sheet=dummy_sprite_sheet,
+        SpritePerRow=4, SpriteWidth=32, SpriteHeight=32
+    )
+    bird.kill()
+    assert bird.alive is False
+
+def test_check_collision(dummy_sprite_sheet):
+    bird = Bird(
+        value=10, x=50, y=50, speed=2, hp=3,
+        DirectionTimeChange=1.0,
+        right=False, left=False, up=False, down=False,
+        sprite_sheet=dummy_sprite_sheet,
+        SpritePerRow=4, SpriteWidth=32, SpriteHeight=32
+    )
+    inside_point = (55, 55)
+    outside_point = (10, 10)
+    assert bird.check_collision(inside_point) is True
+    assert bird.check_collision(outside_point) is None
+
+def test_update_position_changes(dummy_sprite_sheet):
+    bird = Bird(
+        value=10, x=100, y=100, speed=1, hp=3,
+        DirectionTimeChange=0,
+        right=True, left=False, up=False, down=False,
+        sprite_sheet=dummy_sprite_sheet,
+        SpritePerRow=4, SpriteWidth=32, SpriteHeight=32
+    )
+    bird.update()
+    assert bird.x < 100
